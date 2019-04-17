b2fe40918f533888bef4ba6ac8c1225ed219d9d2
Harirak Intarak <meen_harirak@hotmail.com>
2019-04-17 07:36:43 +0700

    Moved 'fortune cookie' functionally into module

--- index.js
+++ index.js
@@ -1,16 +1,11 @@
 var express = require('express');
 var app = express();
 
-var fortunes = [
-    "Conquer your fears or they will conquer you",
-    "Rivers need springs.",
-    "Do not fear what you don't know.",
-    "You will have a pleasant surprise.",
-    "Whenever possible, keep it simple.",
-]
 var handlebars = require('express-handlebars').create({defaultLayout:'main'});
 app.engine('handlebars',handlebars.engine);
 app.set('view engine','handlebars');
+
+var fortune = require('./lib/fortune.js')
 
 app.set('port',process.env.PORT||3000);
 
@@ -20,8 +15,7 @@ app.get('/',function(req,res){
     res.render('home');
 });
 app.get('/about',function(req,res){
-    var randomFortune = fortunes[Math.floor(Math.random()*fortunes.length)];
-    res.render('about',{fortune: randomFortune});
+    res.render('about',{fortune: fortune.getFortune()});
 });
 
 
